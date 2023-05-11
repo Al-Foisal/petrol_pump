bdd7b5af4bcf6c4e60819a4e8422b0ba608a50f0
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-05-11 09:51:10 +0600

    login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,84 +1,181 @@
-@extends('layouts.app_backup')
-@section('css')
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+
+<!-- Mirrored from themes.pixelstrap.com/fastkart/back-end/create-coupon.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 30 Apr 2023 06:48:55 GMT -->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="Fastkart admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, Fastkart admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+    <title>Fastkart - Create Coupon</title>
+
+    <!-- Google font-->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+
+    <!-- Fontawesome css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/font-awesome.css">
+
+    <!-- Linear Icon css -->
+    <link rel="stylesheet" href="assets/css/linearicon.css">
+
+    <!-- remixicon css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/remixicon.css">
+
+    <!-- Themify icon css-->
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/themify.css">
+
+    <!-- Feather icon css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/feather-icon.css">
+
+    <!-- Plugins css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/scrollbar.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/animate.css">
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/bootstrap.css">
+
+    <!-- App css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <style>
-        .modify-card-style {
-            background: unset;
-            border: none;
+        .page-wrapper .page-body-wrapper {
+            background-color: unset;
+        }
+
+        .page-wrapper.compact-wrapper .page-body-wrapper .page-body {
+            min-height: calc(100vh - 80px);
+            margin-top: 90px;
+            margin-left: 280px;
+            background-color: unset;
+            padding-top: 25px;
+            padding-bottom: 54px !important;
+        }
+
+        .new-card {
+            width: 600px;
+            margin: 0 auto;
+            /* height: 100vh; */
+        }
+
+        .new-card .card {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
         }
     </style>
-@endsection
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card ">
-                    <div class="card-header text-center">{{ config('app.name') }}</div>
+</head>
 
+<body>
+
+
+    <!-- page-wrapper Start-->
+    <div class="row"
+        style="position: relative;height:100vh;background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(../maxresdefault.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;">
+        <div class="col-lg-12">
+            <div class="new-card">
+                <div class="card">
                     <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                        <div class="title-header option-title">
+                            <h5>Login to start your session</h5>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="email">{{ __('Email Address') }}</label>
+                        <div class="tab-content" id="pills-tabContent">
+                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel">
+                                <form class="theme-form theme-form-2 mega-form" action="{{ route('login') }}"
+                                    method="post">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="mb-4 form-group">
+                                            <label>Email</label>
+                                            <div>
+                                                <input id="email" type="email"
+                                                    class="form-control @error('email') is-invalid @enderror"
+                                                    name="email" value="{{ old('email') }}" required
+                                                    autocomplete="email" autofocus>
+                                            </div>
+                                        </div>
 
-                                <div>
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        <div class="mb-4 form-group">
+                                            <label>Password</label>
+                                            <div>
+                                                <input id="password" type="password"
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    name="password" required autocomplete="current-password">
+                                            </div>
+                                        </div>
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                                        <div class="form-group  text-center" style="left: 38%;">
+                                            <input class="btn btn-primary " style="letter-spacing: 3px;"
+                                                type="submit" value="Login">
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
 
-                            <div class="form-group">
-                                <label for="password">{{ __('Password') }}</label>
-
-                                <div>
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            {{-- 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div> --}}
-
-                            <div class="row mb-0">
-                                <div class="">
-                                    <button type="submit" class="btn btn-block btn-primary w-100">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    {{-- @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif --}}
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-@endsection
+    <!-- page-wrapper end-->
+
+
+
+    <!-- latest js -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+
+    <!-- feather icon js -->
+    <script src="assets/js/icons/feather-icon/feather.min.js"></script>
+    <script src="assets/js/icons/feather-icon/feather-icon.js"></script>
+
+    <!-- scrollbar simplebar js -->
+    <script src="assets/js/scrollbar/simplebar.js"></script>
+    <script src="assets/js/scrollbar/custom.js"></script>
+
+    <!-- customizer js -->
+    <script src="assets/js/customizer.js"></script>
+
+    <!-- Sidebar js -->
+    <script src="assets/js/config.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="assets/js/sidebar-menu.js"></script>
+    <script src="assets/js/notify/bootstrap-notify.min.js"></script>
+    <script src="assets/js/notify/index.js"></script>
+
+    <!--Dropzon js -->
+    <script src="assets/js/dropzone/dropzone.js"></script>
+    <script src="assets/js/dropzone/dropzone-script.js"></script>
+
+    <!-- select 2 js -->
+    <script src="assets/js/select2.min.js"></script>
+    <script src="assets/js/select2-custom.js"></script>
+
+    <!-- sidebar effect -->
+    <script src="assets/js/sidebareffect.js"></script>
+
+    <!-- Theme js -->
+    <script src="assets/js/script.js"></script>
+</body>
+
+
+<!-- Mirrored from themes.pixelstrap.com/fastkart/back-end/create-coupon.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 30 Apr 2023 06:48:55 GMT -->
+
+</html>
