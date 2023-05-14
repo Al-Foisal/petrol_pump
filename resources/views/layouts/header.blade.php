247a16481dcec3b4833874bd308ea69838151082
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-05-14 13:13:39 +0600

    dashboard link update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,14 +3,14 @@
     <div class="header-wrapper m-0">
         <div class="header-logo-wrapper p-0">
             <div class="logo-wrapper">
-                <a href="index.html">
+                <a href="{{ route('dashboard') }}">
                     <img class="img-fluid main-logo" src="{{ asset('assets/images/logo/1.png') }}" alt="logo">
                     <img class="img-fluid white-logo" src="{{ asset('assets/images/logo/1-white.png') }}" alt="logo">
                 </a>
             </div>
             <div class="toggle-sidebar">
                 <i class="status_toggle middle sidebar-toggle" data-feather="align-center"></i>
-                <a href="index.html">
+                <a href="{{ route('dashboard') }}">
                     <img src="{{ asset('assets/images/logo/1.png') }}" class="img-fluid" alt="">
                 </a>
             </div>
@@ -33,7 +33,9 @@
                         <div class="user-name-hide media-body">
                             <span>{{ auth()->user()->name }}</span>
                             <p class="mb-0 font-roboto">
-                                @if (!empty(auth()->user()->getRoleNames()))
+                                @if (
+                                    !empty(auth()->user()->getRoleNames()
+                                    ))
                                     @foreach (auth()->user()->getRoleNames() as $v)
                                         {{ $v }}
                                     @endforeach
